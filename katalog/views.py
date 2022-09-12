e84545109c0aa87a3675d55424f424a121642b2b
thobyasmuda <silalahithobyas@gmail.com>
2022-09-12 13:38:43 +0700

    modifs

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Kak Thoby',
+        'studentid': '2106650001'
+}
+    return render(request, "katalog.html", context)
